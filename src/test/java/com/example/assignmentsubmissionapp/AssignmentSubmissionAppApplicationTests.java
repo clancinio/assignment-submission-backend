d8b54b9161ff68848fe8c167bfa1771e22994640
Clancinio <dean.clancy91@gmail.com>
2022-07-01 16:31:21 +0100

    Initial commit - Set up and connect to DB

---
+++ src/test/java/com/example/assignmentsubmissionapp/AssignmentSubmissionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmentsubmissionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
