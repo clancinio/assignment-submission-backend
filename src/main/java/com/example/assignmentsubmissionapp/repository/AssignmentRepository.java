f53c2fad864111518ce36f49efaef3f538ec007c
Clancinio <dean.clancy91@gmail.com>
2022-07-25 16:26:26 +0100

    Update query

--- src/main/java/com/example/assignmentsubmissionapp/repository/AssignmentRepository.java
+++ src/main/java/com/example/assignmentsubmissionapp/repository/AssignmentRepository.java
@@ -13,6 +13,8 @@ public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
 
     List<Assignment> findByUser(User user);
 
-    @Query("select a from Assignment a where a.status = 'Submitted'")
-    List<Assignment> findByCodeReviewer(User user);
+    @Query("select a from Assignment a "
+            + "where (a.status = 'submitted' and (a.codeReviewer is null or a.codeReviewer = :codeReviewer))"
+            + "or a.codeReviewer = :codeReviewer")
+    List<Assignment> findByCodeReviewer(User codeReviewer);
 }
